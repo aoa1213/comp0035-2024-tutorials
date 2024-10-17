0bd617768187d2e822fc333156d61769d44ed2c4
aoa1213 <2195783877@qq.com>
2024-10-17 19:34:34 +0100

    Activity 2.5

--- src/data_peparation.py
+++ src/data_peparation.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from pathlib import Path
-
+import numpy as np
 
 def readfile():
     """Summary or Description of the Function
@@ -35,7 +35,6 @@ def describe_dataframe(locationarray,dataframearray):
         dataframe = dataframearray[i]
         column = dataframe.columns
         dtypes = dataframe.dtypes
-        info = dataframe.info()
         describe = dataframe.describe()
         number = {0:"first" ,1:"second", 2:"third"}
         print(f"The {number[i]} dataframe is \n{dataframe}")
@@ -43,13 +42,43 @@ def describe_dataframe(locationarray,dataframearray):
         print(f"The last five rows are: \n{dataframe.tail()}")
         print(f"The column labels are \n{column}")
         print(f"The data types of columon are: \n{dtypes}")
-        print(f"The informaiton: \n{info} ")
+        dataframe.info()
         print(f"The {describe}")
 
-def data_preparation():
+def data_preparation(dataframearray):
+    # Convert a specific column from float64 to int
+    df_event = dataframearray[0]
+    df_medal = dataframearray[2]
+    df_event_columns_to_change = ['countries', 'events', 'participants_m', 'participants_f', 'participants']
+    df_medal_columns_to_change = ['Rank']
+    print(df_event['start'])
+    print(df_event['end'])
+    if 'start' in df_event.columns:
+        df_event['start'] = pd.to_datetime(df_event['start'], format='%d/%m/%Y')
+    if 'start' in df_medal.columns:
+        df_medal['start'] = pd.to_datetime(df_medal['start'], format='%d/%m/%Y')
+
+    for col in df_event_columns_to_change:
+        if col in df_event.columns:
+            #df_event[col] = pd.to_numeric(df_event[col], errors='coerce')  # 转换为数字
+            df_event[col] = df_event[col].fillna(0)  # fill in the NaN value with 0
+            df_event[col] = df_event[col].replace([np.inf, -np.inf], 0)  # replace inf and -inf with 0
+            df_event[col] = df_event[col].astype(int)  # convert into int
+
+    for col in df_medal_columns_to_change:
+        if col in df_medal.columns:
+            #df_medal[col] = pd.to_numeric(df_medal[col], errors='coerce')  # 转换为数字
+            df_medal[col] = df_medal[col].fillna(0)
+            df_medal[col] = df_medal[col].replace([np.inf, -np.inf], 0)
+            df_medal[col] = df_medal[col].astype(int)
+
+
+
+
     
 
 if __name__ == '__main__':
     locationarray,dataframearray=readfile()
     describe_dataframe(locationarray,dataframearray)
+    data_preparation(dataframearray)
 
