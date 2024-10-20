277ab4a74beb3411bf0ae32b7aa9c21c24edbc9c
aoa1213 <2195783877@qq.com>
2024-10-20 20:22:45 +0100

    complete week 2

--- src/data_peparation.py
+++ src/data_peparation.py
@@ -15,23 +15,27 @@ def readfile():
     try:
         # print(Path.cwd())
         # print(Path(__file__))
-        data_file_location_1 = Path(__file__).parent.joinpath("tutorialpkg", "data", "paralympics_events_raw.csv")
-        dataframe_paralympics_events_raw = pd.read_csv(data_file_location_1)
-        data_file_location_2 = Path(__file__).parent.joinpath("tutorialpkg", "data", "paralympics_all_raw.xlsx")
-        dataframe_paralympics_all_raw = pd.read_excel(data_file_location_2)
-        dataframe_page2 = pd.read_excel(data_file_location_2, sheet_name="medal_standings")
+        cols = ['type', 'year', 'country', 'host', 'start', 'end', 'countries', 'events', 'sports',
+        'participants_m', 'participants_f', 'participants']
+        event_raw_location = Path(__file__).parent.joinpath("tutorialpkg", "data", "paralympics_events_raw.csv")
+        df_paralympics_events_raw = pd.read_csv(event_raw_location,usecols=cols)
 
+        all_raw_location = Path(__file__).parent.joinpath("tutorialpkg", "data", "paralympics_all_raw.xlsx")
+        df_paralympics_all_raw = pd.read_excel(all_raw_location,usecols=cols)
+
+
+        df_paralympics_all_raw_page2 = pd.read_excel(all_raw_location, sheet_name="medal_standings")
         # put all locaions and dataframe into a array 
-        locationarray = [data_file_location_1, data_file_location_2, data_file_location_2]
-        dataframearray = [dataframe_paralympics_events_raw, dataframe_paralympics_all_raw, dataframe_page2]
+
+        locationarray = [event_raw_location, all_raw_location, all_raw_location]
+        dataframearray = [df_paralympics_events_raw, df_paralympics_all_raw, df_paralympics_all_raw_page2]
         return locationarray, dataframearray
     except:
         print("File not exsist")
 
 
-def describe_dataframe(locationarray, dataframearray):
+def describe_dataframe(dataframearray):
     for i in range(len(dataframearray)):
-        location = locationarray[i]
         dataframe = dataframearray[i]
         column = dataframe.columns
         dtypes = dataframe.dtypes
@@ -51,10 +55,12 @@ def data_preparation(dataframearray):
     df_medal = dataframearray[2]
     df_event_columns_to_change = ['countries', 'events', 'participants_m', 'participants_f', 'participants']
     df_medal_columns_to_change = ['Rank']
-    print(df_event['start'])
-    print(df_event['end'])
+
+    # print(df_event['start'])
+    # print(df_event['end'])
     if 'start' in df_event.columns:
         df_event['start'] = pd.to_datetime(df_event['start'], format='%d/%m/%Y')
+
     if 'start' in df_medal.columns:
         df_medal['start'] = pd.to_datetime(df_medal['start'], format='%d/%m/%Y')
 
@@ -73,11 +79,63 @@ def data_preparation(dataframearray):
             df_medal[col] = df_medal[col].astype(int)
 
 
+def add_new_column(dataframearray):
+    df = dataframearray[1]
+    df.insert(df.columns.get_loc('end'), 'duration', df['end'] - df['start'])
+    df["duration"] = df["duration"].dt.days.astype(int)
+    dataframearray[1] = df
+    return dataframearray
+
+def remove_column_strip(dataframearray):
+    df_e = dataframearray[0]
+    df_e = df_e.drop(index=[0,17,31])
+    df_e = df_e.reset_index(drop=True)
+    print(df_e['type'].unique())
+    df_e['type'] = df_e['type'].str.strip().str.lower()
+    # df_striped = df_e.str.strip()
+    # df_lowercase_striped = df_striped.str.lower()
+    dataframearray[0]=df_e
+    return dataframearray
+
+def replace_country_name(dataframearray):
+    df_e = dataframearray[0]
+    replacement_names = {
+    'UK': 'Great Britain',
+    'USA': 'United States of America',
+    'Korea': 'Republic of Korea',
+    'Russia': 'Russian Federation',
+    'China': "People's Republic of China"
+    }
+    df_e = df_e.replace(to_replace=replacement_names)
+    dataframearray[0]=df_e
+    return dataframearray
+
+def dataframe_combine(dataframearray):
+    df_e = dataframearray[0]
+    df_e_f = df_e.loc[:,['country']]
+
+    data_npc = Path(__file__).parent.joinpath("tutorialpkg", 'data', 'npc_codes.csv')
+    df_npc = pd.read_csv(data_npc, encoding='utf-8', encoding_errors='ignore', usecols=['Code', 'Name'])
+
+    df_result = df_e_f.merge(df_npc, how='left', left_on='country', right_on='Name')
+
+    dataframearray[0] = df_result
+    return dataframearray
+
+def output_csv(dataframearray):
+        location = Path(__file__).parent.joinpath("tutorialpkg","data","paralympics_events_prepared.csv")
+        dataframearray[0].to_csv(location, index=False)
+
+
 
     
 
 if __name__ == '__main__':
     locationarray,dataframearray = readfile()
-    describe_dataframe(locationarray, dataframearray)
-    data_preparation(dataframearray)
+    dataframearray_altered = add_new_column(dataframearray)
+    describe_dataframe(dataframearray_altered)
+    data_preparation(dataframearray_altered)
 
+    combined_dataframe_array = dataframe_combine(dataframearray_altered)
+    output_csv(combined_dataframe_array)
+    
